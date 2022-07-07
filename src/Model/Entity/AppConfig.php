23385cb3f4bf4f18ef468495609c0a3093ef6773
stefanorosanelli <stefano.rosanelli@gmail.com>
2022-07-07 17:38:13 +0200

    refactor: use new `AppConfig` table

---
+++ src/Model/Entity/AppConfig.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2022 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Model\Entity;
+
+/**
+ * App Config Entity.
+ *
+ * {@inheritDoc}
+ * @since 5.0.0
+ */
+class AppConfig extends Config
+{
+    /**
+     * @inheritDoc
+     */
+    protected $_accessible = [
+        '*' => true,
+        'application_id' => false,
+        'context' => false,
+        'created' => false,
+        'modified' => false,
+    ];
+}
